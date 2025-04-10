b46e201699ec5db608b64d30b7e7d5351489a7ad
javaDev <javaDev@DESKTOP-RLMBFFT>
2025-04-10 21:05:50 +0900

    구구단 예제 관련 이것저것 가지고 놈

--- jse_20250410/src/com/javateam/jse/TimeTable.java
+++ jse_20250410/src/com/javateam/jse/TimeTable.java
@@ -1,4 +1,5 @@
 package com.javateam.jse;
+import java.util.Scanner;
 
 public class TimeTable {
 
@@ -19,23 +20,30 @@ public class TimeTable {
 		int FirstNum ; // 구구단 앞 번호에 대한 초기화
 		int LastNum; // 구구단 뒷 번호에 대한 초기화
 		
+		System.out.print("몇단 까지 출력할지를 입력하세요 : ");
+		Scanner inputNum = new Scanner(System.in);
+		int NumLayer = inputNum.nextInt();		
+		inputNum.close();		
+		
 		//구구단 반복문 시작
-		for(FirstNum=1; FirstNum <=19; FirstNum++) { 
-		// 앞 단수 for문 / 2단부터 시작
-			for(LastNum=1; LastNum<=19; LastNum++) { 
-			// 뒷 단수 for문
-				int comp = FirstNum * LastNum;
-				System.out.println(FirstNum+" * "+ LastNum + " = " + comp);
+		for(FirstNum=1; FirstNum <= NumLayer; FirstNum++) { 
+			// 앞 단수 for문
+			for(LastNum=1; LastNum <= NumLayer; LastNum++) { 
+				// 뒷 단수 for문
+				System.out.println(FirstNum+" * "+ LastNum + " = " + (FirstNum * LastNum));
 			}
 			
-			// 앞 단 변경될 때 구분을 위함. // 마지막 19단은 해당x
-			if(FirstNum != 19){
-				System.out.println("");
-				System.out.println("=================");
-				System.out.println("");
-			}else {
-				break;
-			}
+			// 앞 단 변경될 때 구분을 위함. // 마지막 단은 출력 끝 출력
+//			if(FirstNum != NumLayer){
+//				System.out.println("");
+//				System.out.println("=================");
+//				System.out.println("");
+//			}else {
+//				System.out.println("====출력 끝====");
+//				break;
+//			}
+			// 9999단 입력시 시간: 
+			// 1) 8분 52초 724 / 2) 8분 48초 029 / 3)8분 42초 30
 			
 		}// for end
 
