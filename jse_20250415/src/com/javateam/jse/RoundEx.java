b1372e13a10561df78d4052120eb75e6c5ea64ba
javaDev <javaDev@DESKTOP-RLMBFFT>
2025-04-16 13:48:05 +0900

    math.pow 학습

--- jse_20250415/src/com/javateam/jse/RoundEx.java
+++ jse_20250415/src/com/javateam/jse/RoundEx.java
@@ -15,15 +15,18 @@ public class RoundEx {
 		// System.out.println(Math.round(num * 1E1) / 1E1f);
 		System.out.println(Math.round(num * Math.pow(10, 1)) / Math.pow(10, 1));
 		
+		
 		// 소숫점 3째자리에서 반올림 => 정수/소숫점 2자리 표현
 		// System.out.println(Math.round(num * 100) / 100f);
 		// System.out.println(Math.round(num * 1E2) / 1E2f);
 		System.out.println(Math.round(num * Math.pow(10, 2)) / Math.pow(10, 2));
 		
+		
 		// 소숫점 4째자리에서 반올림 => 정수/소숫점 3자리 표현
 		// System.out.println(Math.round(num * 1000) / 1000f);
 		// System.out.println(Math.round(num * 1E3) / 1E3f);
 		System.out.println(Math.round(num * Math.pow(10, 3)) / Math.pow(10, 3));
+		
 		
 		// 승수(square) 표현 Math.pow() 메소드(함수) 활용
 		System.out.println(Math.pow(10, 1));
